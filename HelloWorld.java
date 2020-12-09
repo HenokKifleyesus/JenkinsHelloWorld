96bed3aa18568edb3be3d16fe303fdbaf5c83041
HenokKifleyesus <64043964+HenokKifleyesus@users.noreply.github.com>
2020-12-09 04:37:14 +0000

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ public class HelloWorld{
 
 	public static void main(String [] args){
 
-		System.out.println("This is Henok Kifleyesus's jenkins pipeline Project");
+		System.out.println("This is managed by Henok Kifleyesus's jenkins pipeline Project");
 	}
 }
